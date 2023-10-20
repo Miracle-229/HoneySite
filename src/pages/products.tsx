a97776a0ed5d287f48876bcd6ffeb76050a71933
Miracle-229 <zzzzhd02@gmail.com>
2023-10-20 18:14:55 +0300

    getting started with creating a product page

---
+++ src/pages/products.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../layouts/Layout';
+import style from '../styles/Home.module.scss';
+
+function Products() {
+  return (
+    <Layout title="Продукты">
+      <h3 className={style.h3}>Продукты</h3>
+    </Layout>
+  );
+}
+
+export default Products;
